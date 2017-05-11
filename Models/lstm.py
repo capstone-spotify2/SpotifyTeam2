4de2ac6681d3c37ea4b09d9ed58e9ef350369f65
Heidi Chen <s.heidi.chen@gmail.com>
2017-05-11 15:31:01 -0400

    flake8

--- Models/lstm.py
+++ Models/lstm.py
@@ -1,22 +1,18 @@
 import re
 import pandas as pd
-import nltk.data
-from os import listdir
-from os.path import isfile, join
-from nltk import word_tokenize
-import gensim
-from gensim.models import Word2Vec
-from gensim.models import word2vec
-from gensim.models import Phrases
-import logging
 from nltk.corpus import stopwords
-from nltk.tokenize import RegexpTokenizer
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers.embeddings import Embedding
+from keras.preprocessing import sequence
+from keras.utils import np_utils
 
 def sentence_to_wordlist(sentence, remove_stopwords=False):
     # 1. Remove non-letters
-    sentence_text = re.sub(r'[^\w\s]','', sentence)
+    sentence_text = re.sub(r'[^\w\s]', '', sentence)
     # 2. Convert words to lower case and split them
     words = sentence_text.lower().split()
     if remove_stopwords:
@@ -25,50 +21,34 @@ def sentence_to_wordlist(sentence, remove_stopwords=False):
     # 3. Return a list of words
     return(words)
 
-
-##############################
-#
-# word2vec + LSTM
-#
-#####
-
-from keras.datasets import imdb
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import LSTM
-from keras.layers.embeddings import Embedding
-from keras.engine import Input
-from keras.preprocessing import sequence
-
-## load pre-trained word2vec dataset
+# load pre-trained word2vec dataset
 from gensim.models.keyedvectors import KeyedVectors
 word2vec = KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)
-
 
 weights = word2vec.syn0
 layer = Embedding(input_dim=weights.shape[0], output_dim=weights.shape[1], weights=[weights])
 vocab = dict([(k, v.index) for k, v in word2vec.vocab.items()])
 
-## load matched song lyrics with tags
-tagdf = pd.read_csv('full_dataset.csv', index_col = 0)
+# load matched song lyrics with tags
+tagdf = pd.read_csv('full_dataset.csv', index_col=0)
 newind = np.arange(tagdf.shape[0])
-## randomly shuffle
+# randomly shuffle
 np.random.shuffle(newind)
 
-## split out training, testing, and validation set
-train_df = tagdf.ix[newind[:int(tagdf.shape[0]*0.9*0.7)]]
-test_df = tagdf.ix[newind[int(tagdf.shape[0]*0.9*0.7):int(tagdf.shape[0]*0.9)]]
-validate_df = tagdf.ix[newind[int(tagdf.shape[0]*0.9):]]
+# split out training, testing, and validation set
+train_df = tagdf.ix[newind[:int(tagdf.shape[0] * 0.9 * 0.7)]]
+test_df = tagdf.ix[newind[int(tagdf.shape[0] * 0.9 * 0.7):int(tagdf.shape[0] * 0.9)]]
+validate_df = tagdf.ix[newind[int(tagdf.shape[0] * 0.9):]]
 
 
 x_train = []
 i = 0
 for lyrics in train_df.Song_lyrics:
     wordlist = sentence_to_wordlist(lyrics)
-    vecinput = [vocab[w] for w in wordlist if w in vocab.keys()]
+    vecinput = [vocab[w] for w in wordlist if w in vocab]
     x_train.append(vecinput)
     i += 1
-    if i%100 == 0:
+    if i % 100 == 0:
         print (i)
 
 
@@ -76,10 +56,10 @@ x_test = []
 i = 0
 for lyrics in test_df.Song_lyrics:
     wordlist = sentence_to_wordlist(lyrics)
-    vecinput = [vocab[w] for w in wordlist if w in vocab.keys()]
+    vecinput = [vocab[w] for w in wordlist if w in vocab]
     x_test.append(vecinput)
     i += 1
-    if i%100 == 0:
+    if i % 100 == 0:
         print (i)
 
 
@@ -87,26 +67,25 @@ x_all = []
 i = 0
 for lyrics in tagdf.Song_lyrics:
     wordlist = sentence_to_wordlist(lyrics)
-    vecinput = [vocab[w] for w in wordlist if w in vocab.keys()]
+    vecinput = [vocab[w] for w in wordlist if w in vocab]
     x_all.append(vecinput)
     i += 1
-    if i%100 == 0:
+    if i % 100 == 0:
         print (i)
 
 
 x_train = np.array(x_train).reshape((train_df.shape[0],))
 x_test = np.array(x_test).reshape((test_df.shape[0],))
-x_train2 =sequence.pad_sequences(x_train, 150)
-x_test2 =sequence.pad_sequences(x_test, 150)
+x_train2 = sequence.pad_sequences(x_train, 150)
+x_test2 = sequence.pad_sequences(x_test, 150)
 
 x_all = np.array(x_all).reshape((tagdf.shape[0],))
-x_all2 =sequence.pad_sequences(x_all, 150)
-
+x_all2 = sequence.pad_sequences(x_all, 150)
 
 
 alltags = tagdf.tag.unique()
 
-## for binary model
+# for binary model
 
 # tag = 'happy'
 # tagmap = {alltags[i]: i for i in range(len(alltags))}
@@ -123,7 +102,7 @@ alltags = tagdf.tag.unique()
 # pred = model.predict(x_test2)
 # np.sum(pred.T>0.5)
 
-## for multiclass model
+# for multiclass model
 
 encoder = LabelEncoder()
 encoder.fit(train_df.tag)
@@ -149,27 +128,29 @@ model.add(layer)
 model.add(LSTM(100))
 model.add(Dense(len(alltags), activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-model.fit(x_train2, y_train, validation_data=(x_test2, y_test), epochs = 3)
+model.fit(x_train2, y_train, validation_data=(x_test2, y_test), epochs=3)
 # model.save('multiclass_model.h5')
 
 # Final evaluation of the model
 scores = model.evaluate(x_all2, y_all, verbose=0)
-print("Accuracy: %.2f%%" % (scores[1]*100))
+print("Accuracy: %.2f%%" % (scores[1] * 100))
 
-## merge to output predicted probabilities
+# merge to output predicted probabilities
 pred = model.predict(x_all2)
-preddf = pd.DataFrame(pred, columns = encoder.classes_)
-tagdf = tagdf.drop(['index'], axis = 1)
-preddf['id'] = tagdf['id']
-preddf = tagdf.merge(preddf, on = 'id')
+preddf = pd.DataFrame(pred, columns=encoder.classes_)
+tagdf = tagdf.drop(['index'], axis=1)
+preddf['id'] = tagdf['id'].values
+preddf = tagdf.merge(preddf, on='id')
 # ipred = pd.DataFrame(pred).apply(np.argmax, axis = 1)
 # tpred = ipred.map(lambda x: encoder.classes_[x])
 # preddf['predicted_1'] = ipred.map(lambda x: encoder.classes_[x])
 from scipy.stats import rankdata
 for i in range(1, 4):
-    preddf['predicted_%s'%i] = pd.DataFrame(pred).apply(lambda x: list(rankdata(x)).index(len(alltags) - i + 1), axis = 1)
-    preddf['predicted_%s'%i] = preddf['predicted_%s'%i].map(lambda x: encoder.classes_[x])
-print("Accuracy: %.2f%%" % (np.sum(preddf.tag == preddf.predicted_1)/preddf.shape[0]*100))
-print("Top 3 Accuracy: %.2f%%" % (np.sum(preddf.apply(lambda x: x.tag in x[['predicted_%s'%i for i in range(1, 4)]].values, axis = 1))/preddf.shape[0]*100))
+    preddf['predicted_%s' % i] = pd.DataFrame(pred).apply(
+        lambda x: list(rankdata(x)).index(len(alltags) - i + 1), axis=1)
+    preddf['predicted_%s' % i] = preddf['predicted_%s' % i].map(lambda x: encoder.classes_[x])
+print("Accuracy: %.2f%%" % (np.sum(preddf.tag == preddf.predicted_1) / preddf.shape[0] * 100))
+print("Top 3 Accuracy: %.2f%%" % (np.sum(preddf.apply(lambda x: x.tag in x[[
+      'predicted_%s' % i for i in range(1, 4)]].values, axis=1)) / preddf.shape[0] * 100))
 
 preddf.to_csv('Models/multiclassLSTMOutput.csv')
