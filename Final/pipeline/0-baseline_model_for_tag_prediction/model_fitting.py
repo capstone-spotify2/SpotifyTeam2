48d341ee323005af7e88ac65e8084ec6d147818d
JunZhou1544 <judyzhou0201@gmail.com>
2017-05-13 01:43:10 -0700

    cleaned code in baseline model tag prediction folder and modified README.md

--- Final/pipeline/0-baseline_model_for_tag_prediction/model_fitting.py
+++ Final/pipeline/0-baseline_model_for_tag_prediction/model_fitting.py
@@ -1,18 +1,15 @@
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import HashingVectorizer
 import cPickle
 import sys
 from optparse import OptionParser
-import csv
-import numpy as np
 
-import nltk
-from nltk import word_tokenize
 
 def strip_non_ascii(string):
     ''' Returns the string without non ASCII characters'''
     stripped = (c for c in string if 0 < ord(c) < 127)
     return ''.join(stripped)
+
+
 def LyricsToSentences(lyrics):
     lyrics = strip_non_ascii(lyrics)
     if '\r' in lyrics:
@@ -23,7 +20,7 @@ def LyricsToSentences(lyrics):
         lines = ' '.join(lines).replace("\n \n", "\n").split(' \n')
         l = ' '
         for i in lines:
-            l+= i
+            l += i
         return l
     elif "\n\n" in lyrics:
         lines = lyrics.split('\n')
@@ -33,10 +30,11 @@ def LyricsToSentences(lyrics):
         lines = ' '.join(lines).split('\r')
         l = ' '
         for i in lines:
-            l+= i
+            l += i
         return l
 
-def model_fit(input_song, model, feature_num = 150):
+
+def model_fit(input_song, model, feature_num=150):
     print("Process the input song...")
     with open(input_song, 'rb') as f:
         fa = LyricsToSentences(f.read())
@@ -57,12 +55,13 @@ def model_fit(input_song, model, feature_num = 150):
     print("The prediction results are:")
     res = []
     for i in range(3):
-        tag,prob = sorted(rank, key=lambda x: -x[1])[i]
+        tag, prob = sorted(rank, key=lambda x: -x[1])[i]
         print tag
         res.append(tag)
     return res
-    
+
 # RF_short = RandomForestClassifier(n_estimators = 15, max_depth =5)
+
 
 def main(argv=None):
     if argv is None:
@@ -71,26 +70,30 @@ def main(argv=None):
     usage = "Usage: %prog song_input.txt (pass -h for more info)"
     parser = OptionParser(usage)
 
-    parser.add_option("-n", "--n_features", dest="n_features", type = int,
-                  help="num of features required(must equal to the feature num in trained model)")
-    parser.add_option("-m", "--model", dest="model", type = int,
-                  help="trained model")
+    parser.add_option("-n", "--n_features", dest="n_features", type=int,
+                      help="dimension of the hashing vectorizer(must equivalent to n_features in training step)")
+    parser.add_option("-m", "--model", dest="model",
+                      help="trained model")
 
     (options, args) = parser.parse_args(argv[1:])
 
     if len(args) < 1:
         parser.error("Must pass in a feature file")
-    if options.n_features is not None and :
+    if options.n_features is not None and options.model is not None:
+        with open(options.model, 'rb') as f:
+            rf = cPickle.load(f)
+        model_fit(args[0], rf, feature_num=options.n_features)
+    elif options.n_features is not None:
         with open('model.pickle', 'rb') as f:
             rf = cPickle.load(f)
-        model_fit(args[0], rf, feature_num = options.n_features)
+        model_fit(args[0], rf, feature_num=options.n_features)
     else:
         with open('model.pickle', 'rb') as f:
             rf = cPickle.load(f)
         model_fit(args[0], rf)
 
-    
     return 0
+
 
 if __name__ == "__main__":
     sys.exit(main())
